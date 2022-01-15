35cb560f4fd6c1744c4f59df0fb175076c831479
duhee77 <dwd4798@gmail.com>
2022-01-15 21:34:36 +0900

    chap3_react-redux

---
+++ react-redux-tutorial/src/routes/Home.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useState } from 'react'
+import { connect } from 'react-redux'
+import { actionCreators } from '../store'
+
+const Home = ({ toDos, addToDo }) => {
+  const [text, setText] = useState('')
+  const onChange = (e) => {
+    setText(e.target.value)
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    addToDo(text)
+    setText('')
+  }
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input type="text" value={text} onChange={onChange} />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  )
+}
+
+function mapStateToProps(state) {
+  console.log(state)
+  return { toDos: state }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    addToDo: (text) => dispatch(actionCreators.addToDo(text)),
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(Home)
